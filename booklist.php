a5ca1c978b5c4e1a670b8c9c92503e87e8ab0b1c
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-04-23 17:16:37 -0400

    dbase query implement

--- booklist.php
+++ booklist.php
@@ -11,11 +11,12 @@ $display = $_GET["display"]; //
 $format = $_GET["format"];
 $display = $_GET["display"];
 
-echo json_encode('{ "books": [ { "category": "cooking", "year": 2009, "price": 22.00, "title": "Breakfast for Dinner", "author": "Amanda Camp" }, { "category": "cooking", "year": 2010, "price": 75.00, "title": "21 Burgers for the 21st Century", "author": "Stuart Reges" } ] }');
-
-
-
-
+//query for categories
+if (strcmp($display, "categories")==0){
+	$db = dbase_connect();
+	$stmt = "SELECT * FROM category;"
+	$categories = mysqli_query($db,$stmt);
+}
 
 
 
--- bookslist.php
+++
@@ -1,8 +0,0 @@
-<?php
-$display = $_GET["display"]; //get query parameter from client
-
-if(strcmp($display, "categories") == 0){
-	echo "match found"
-}
-
-?>
