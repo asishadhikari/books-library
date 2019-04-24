17e6d7f47059cace1108a405bdecf82f5f4a95d8
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-04-23 23:35:19 -0400

    refactor and update Readme.md

--- Readme.md
+++ Readme.md
@@ -1,9 +1,15 @@
 # Books 
 > A demonstration of use of AJAX/XML/JSON
+##To Build:
+* Create ```User1``` with password ```USER1PASSWORD``` 
+* import ```db.sql``` and grant permission to user1
+* If faced any issues, use ```CREATE_TABLE``` to initialise database ```BOOKS```
+* Then, use ```populate_data.sql``` to load data
+
+
 ## Requirements:
--[ ] A plain text file named Readme.md that contains the instructions to setup your web site from  scratch. If  anysite-specificparameters need be changed in order for your web site to work, make sure the explanation is included in the instructions
--[X] When the web page at books.php is first opened, the javascript program books.js included in  the  web  page fetches the  list  of  book  categories via  Ajax from booklist.php and displays it as specified. 
--[ ] When the "List Books" is clicked, the javascript program books.js fetches the information of  the books in the
-selected categories via Ajax from booklist.php and displays it as in the specified example
--[X] If  books.php is accessed with the parameter "format" set to "json", i.e. the path part of  the URL  is ```books.php?format=json```, the  web  server  should  send  back  data  in  JSONin  all  the following communication. Otherwise, the web server should send back data in XML.
--[X] The book information on the server must be stored in a MySQL database. There should be 5 tables, one for title, year, price, category and author respectively.(You will create dummy book information to store in the database.)
+- [ ] A plain text file named Readme.md that contains the instructions to setup your web site from  scratch. If  anysite-specificparameters need be changed in order for your web site to work, make sure the explanation is included in the instructions
+- [X] When the web page at books.php is first opened, the javascript program books.js included in  the  web  page fetches the  list  of  book  categories via  Ajax from booklist.php and displays it as specified. 
+- [X] When the "List Books" is clicked, the javascript program books.js fetches the information of  the books in the selected categories via Ajax from booklist.php and displays it as in the specified example
+- [X] If  books.php is accessed with the parameter "format" set to "json", i.e. the path part of  the URL  is ```books.php?format=json```, the  web  server  should  send  back  data  in  JSONin  all  the following communication. Otherwise, the web server should send back data in XML.
+- [X] The book information on the server must be stored in a MySQL database. There should be 5 tables, one for title, year, price, category and author respectively.(You will create dummy book information to store in the database.)
--- db.sql
+++ db.sql
@@ -1,46 +1,138 @@
---- Drop all existing tables ---
-CREATE DATABASE IF NOT EXISTS BOOKS;
-USE BOOKS;
----CREATE USER 'USER1'@'localhost' IDENTIFIED BY 'USER1PASSWORD';
----GRANT ALL PRIVILEGES ON BOOKS.* TO 'USER1'@'localhost';    
+--
+-- Current Database: `BOOKS`
+--
 
-DROP TABLE IF EXISTS title;
-DROP TABLE IF EXISTS year;
-DROP TABLE IF EXISTS price;
-DROP TABLE IF EXISTS category;
-DROP TABLE IF EXISTS author;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `BOOKS` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
-create table title(
-    title_id int not null,
-    title_name varchar(255) not null,
-    category_id int,
-    author_id int,
-    primary key (title_id)
-);
+USE `BOOKS`;
 
-create table year (
-    year int(4) not null,
-    title_id int
-);
+--
+-- Table structure for table `author`
+--
 
-create table price (
-    price decimal(4, 2) not null,
-    title_id int
-);
+DROP TABLE IF EXISTS `author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `author` (
+  `author_id` int(11) NOT NULL,
+  `author` varchar(32) NOT NULL,
+  PRIMARY KEY (`author_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table category (
-    category_id int not null,
-    category varchar(32) not null,
-    primary key (category_id)
-);
+--
+-- Dumping data for table `author`
+--
 
-create table author (
-    author_id int not null,
-    author varchar(32) not null,
-    primary key (author_id)
-);
+LOCK TABLES `author` WRITE;
+/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+INSERT INTO `author` VALUES (1,'Nell Fredy'),(2,'Gel Rady'),(3,'Bromain Ropal'),(4,'Komenstein'),(5,'Eplotay'),(6,'Fredman Rk'),(7,'Gilato Poy'),(8,'Rico Fermi');
+/*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `category`
+--
 
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category` varchar(32) NOT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `category`
+--
 
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Sci-Fi'),(2,'Action'),(3,'Romance'),(4,'Autobiography');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `price`
+--
+
+DROP TABLE IF EXISTS `price`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `price` (
+  `price` decimal(4,2) NOT NULL,
+  `title_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `price`
+--
+
+LOCK TABLES `price` WRITE;
+/*!40000 ALTER TABLE `price` DISABLE KEYS */;
+INSERT INTO `price` VALUES (12.00,1),(2.00,2),(31.00,3),(58.00,4),(99.99,5),(99.99,6),(72.00,7),(85.00,8);
+/*!40000 ALTER TABLE `price` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `title`
+--
+
+DROP TABLE IF EXISTS `title`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `title` (
+  `title_id` int(11) NOT NULL,
+  `title_name` varchar(255) NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `author_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`title_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `title`
+--
+
+LOCK TABLES `title` WRITE;
+/*!40000 ALTER TABLE `title` DISABLE KEYS */;
+INSERT INTO `title` VALUES (1,'Once A day',1,1),(2,'Actionica Man',2,2),(3,'Loverman',3,3),(4,'Autobiographica Physica',4,4),(5,'Sci-Fi in New England',1,5),(6,'Action At home',2,6),(7,'Romancay Bromain',3,7),(8,'Automann Builder',4,8);
+/*!40000 ALTER TABLE `title` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `year`
+--
+
+DROP TABLE IF EXISTS `year`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `year` (
+  `year` int(4) NOT NULL,
+  `title_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `year`
+--
+
+LOCK TABLES `year` WRITE;
+/*!40000 ALTER TABLE `year` DISABLE KEYS */;
+INSERT INTO `year` VALUES (1995,1),(1895,2),(1995,3),(1912,4),(2995,5),(1195,6),(1975,7),(2015,8);
+/*!40000 ALTER TABLE `year` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-04-24  3:28:36
